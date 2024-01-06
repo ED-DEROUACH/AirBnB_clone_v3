cdee11521bffd8ad82f2af6f5c8677d533cb6bb4
ED-DEROUACH <oussama.ed-derouach@outlook.com>
2024-01-06 12:17:25 -0800

    up app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,35 +1,42 @@
 #!/usr/bin/python3
-"""comment"""
-from flask import Flask, Blueprint, jsonify
+'''
+Createw Flask app; and register the blueprint app_views to Flask instance app.
+'''
+
+from os import getenv
+from flask import Flask, jsonify
+from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
-from os import environ
-from flask_cors import CORS
 
 app = Flask(__name__)
+
+# enable CORS and allow for origins:
+CORS(app, resources={r'/api/v1/*': {'origins': '0.0.0.0'}})
+
 app.register_blueprint(app_views)
-cors = CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
+app.url_map.strict_slashes = False
 
 
 @app.teardown_appcontext
-def teardown_app(self):
-    """comment"""
+def teardown_engine(exception):
+    '''
+    Removes the current SQLAlchemy Session object after each request.
+    '''
     storage.close()
 
 
+# Error handlers for expected app behavior:
 @app.errorhandler(404)
 def not_found(error):
-    """deffo pleeffofo"""
-    return jsonify({'error': 'Not found'}), 404
+    '''
+    Return errmsg `Not Found`.
+    '''
+    response = {'error': 'Not found'}
+    return jsonify(response), 404
 
 
-if __name__ == "__main__":
-    if environ.get('HBNB_API_HOST') is None:
-        hos = "0.0.0.0"
-    else:
-        hos = environ.get('HBNB_API_HOST')
-    if environ.get('HBNB_API_PORT') is None:
-        por = 5000
-    else:
-        por = environ.get('HBNB_API_PORT')
-    app.run(host=hos, port=por, threaded=True)
+if __name__ == '__main__':
+    HOST = getenv('HBNB_API_HOST', '0.0.0.0')
+    PORT = int(getenv('HBNB_API_PORT', 5000))
+    app.run(host=HOST, port=PORT, threaded=True)
