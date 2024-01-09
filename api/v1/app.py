1f1caf9421b734a46621314c025c744a69eb0e8e
2oothpick <kobbyboateng14@gmail.com>
2024-01-09 02:57:09 +0000

    Updated app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,25 +1,39 @@
 #!/usr/bin/python3
-"""This is an v1 API module"""
+"""
+Createw Flask app; and register the blueprint app_views to Flask instance app.
+"""
+
+from os import getenv
 from flask import Flask, jsonify, make_response
+from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
-from os import getenv
-from flask_cors import CORS
+
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+
+# enable CORS and allow for origins:
 CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
+#app.url_map.strict_slashes = False
+
+
 @app.teardown_appcontext
-def close_db(db):
-    """Closes current db session"""
+def teardown_engine(exception):
+    """
+    Removes the current SQLAlchemy Session object after each request.
+    """
     storage.close()
 
 
+# Error handlers for expected app behavior:
 @app.errorhandler(404)
-def not_found_404(error):
-    """Handles 404 errors"""
+def not_found(error):
+    """
+    Return errmsg `Not Found`.
+    """
     return make_response(jsonify({"error": "Not found"}), 404)
 
 
