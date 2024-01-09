17b5f04e109896bfcfbc7dd617f158a4ef112bd2
2oothpick <kobbyboateng14@gmail.com>
2024-01-09 02:40:36 +0000

    Updated app.py Fix not_found method

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-'''
+"""
 Createw Flask app; and register the blueprint app_views to Flask instance app.
-'''
+"""
 
 from os import getenv
 from flask import Flask, jsonify, make_response
@@ -13,30 +13,30 @@ from api.v1.views import app_views
 app = Flask(__name__)
 
 # enable CORS and allow for origins:
-CORS(app, resources={r'/api/v1/*': {'origins': '0.0.0.0'}})
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 app.register_blueprint(app_views)
-app.url_map.strict_slashes = False
+#app.url_map.strict_slashes = False
 
 
 @app.teardown_appcontext
 def teardown_engine(exception):
-    '''
+    """
     Removes the current SQLAlchemy Session object after each request.
-    '''
+    """
     storage.close()
 
 
 # Error handlers for expected app behavior:
 @app.errorhandler(404)
 def not_found(error):
-    '''
+    """
     Return errmsg `Not Found`.
-    '''
+    """
     return make_response(jsonify({"error": "Not found"}), 404)
 
 
 if __name__ == '__main__':
     HOST = getenv('HBNB_API_HOST', '0.0.0.0')
-    PORT = int(getenv('HBNB_API_PORT', 5000))
+    PORT = getenv('HBNB_API_PORT', 5000)
     app.run(host=HOST, port=PORT, threaded=True)
